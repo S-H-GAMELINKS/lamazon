538a29975280caca12915e3e097edcc1eb550afa
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-26 11:06:09 +0900

    fix number form

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,6 +1,6 @@
 {{Form::open(['route' => 'products.store'])}}
     {{Form::input('text', 'name')}}
     {{Form::textarea('description')}}
-    {{Form::number('price', 'price', ['min' => 0])}}
+    {{Form::number('price', 0, ['min' => 0])}}
     {{Form::submit('Create')}}
 {{Form::close()}}
