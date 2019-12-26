a9eef45254e0ac940d765e1a2a6e09c4e3795d6a
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-26 12:11:10 +0900

    add relation to product

--- app/Review.php
+++ app/Review.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $fillable = [
+        'content',
+        'product_id',
+    ];
+
+    protected function product()
+    {
+        return $this->belongsTo('App\Product');
+    }
 }
