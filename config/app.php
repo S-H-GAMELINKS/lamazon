6ae900a50b67c150a8cfd4c43d58929d0060d2eb
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-26 17:52:41 +0900

    add service

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Add laravel-follow
+        Overtrue\LaravelFollow\FollowServiceProvider::class,
     ],
 
     /*
