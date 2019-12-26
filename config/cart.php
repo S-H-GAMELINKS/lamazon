b2f522a8501f46ae6b7cad0e5aa53f69f089174b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-26 22:52:57 +0900

    fix cart.php

--- config/cart.php
+++ config/cart.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'tax' => 21,
+    'tax' => 10,
 
     /*
     |--------------------------------------------------------------------------
@@ -56,9 +56,9 @@ return [
 
     'format' => [
 
-        'decimals' => 2,
+        'decimals' => 0,
 
-        'decimal_point' => '.',
+        'decimal_point' => '',
 
         'thousand_separator' => ',',
 
