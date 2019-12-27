fe9fe507f2ba4a68b20d89c946259efc5dd5871a
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-27 16:27:10 +0900

    add jumbtoron

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="jumbotron">
+        <h1 class="display-4">Welcome to Lamazon!</h1>
+        <p class="lead">This is a simple Amazon like service</p>
+        <hr class="my-4">
+        <a class="btn btn-primary btn-lg" href="{{route('products.index')}}" role="button">Show Products</a>
+    </div>
 @endsection
