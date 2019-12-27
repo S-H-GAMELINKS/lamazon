c9a710f786e10692c13553519a6c259d4eb5bc2c
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-27 16:15:20 +0900

    using layouts for category

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,5 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
 {{Form::open(['route' => 'categories.store'])}}
     {{Form::input('text', 'name')}}
     {{Form::textarea('description')}}
     {{Form::submit('create')}}
 {{Form::close()}}
+
+@endsection
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,6 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
 @foreach($categories as $category)
     {{$category->name}}
     {{$category->description}}
 @endforeach
 
 <a href="{{route('categories.create')}}">New</a>
+
+@endsection
