cea74845b9dbfaf90fd343571198b0318a42e6f4
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-13 14:00:48 +0900

    fix show view

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,38 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-    {{$product->name}}
-    {{$product->description}}
-    {{$product->price}}
-    {{$product->category->name}}
 
-@if($product->isFavoritedBy(Auth::user()))
-    {{Form::open(['route' => ['products.fav', $product]])}}
-        {{Form::submit('UnFav')}}
+    <h1 class="display-3">Product Name: {{$product->name}}</h1>
+    <p class="display-4">{{$product->description}}</p>
+    <p class="display-4">Price: {{$product->price}}</p>
+    <p class="display-4">Category: {{$product->category->name}}</p>
+
+    <div class="m-3">
+        @if($product->isFavoritedBy(Auth::user()))
+            {{Form::open(['route' => ['products.fav', $product]])}}
+                {{Form::submit('UnFav', ['class' => 'btn btn-danger'])}}
+            {{Form::close()}}
+        @else
+            {{Form::open(['route' => ['products.fav', $product]])}}
+                {{Form::submit('Fav', ['class' => 'btn btn-primary'])}}
+            {{Form::close()}}
+        @endif
+    </div>
+
+    <div class="m-3">
+        {{Form::open(['route' => 'carts.store'])}}
+            {{Form::hidden('id', $product->id)}}
+            {{Form::hidden('name', $product->name)}}
+            {{Form::hidden('qty', 1)}}
+            {{Form::hidden('price', $product->price)}}
+            {{Form::hidden('weight', 0)}}
+            {{Form::submit('Add', ['class' => 'btn btn-primary'])}}
+        {{Form::close()}}
+    </div>
+
+    @foreach($reviews as $review)
+        <div class="m-2">
+            <p>{{$review->content}}</p>
+        </div>
+    @endforeach
+
+    <div class="m-3">
+    {{Form::open(['route' => ['products.reviews.store', $product]])}}
+        <div class="m-2">
+            {{Form::textarea('content', "", ['class' => 'form-control'])}}
+        </div>
+        {{Form::submit('Create', ['class' => 'btn btn-primary'])}}
     {{Form::close()}}
-@else
-    {{Form::open(['route' => ['products.fav', $product]])}}
-        {{Form::submit('Fav')}}
-    {{Form::close()}}
-@endif
-
-{{Form::open(['route' => 'carts.store'])}}
-    {{Form::hidden('id', $product->id)}}
-    {{Form::hidden('name', $product->name)}}
-    {{Form::hidden('qty', 1)}}
-    {{Form::hidden('price', $product->price)}}
-    {{Form::hidden('weight', 0)}}
-    {{Form::submit('Add')}}
-{{Form::close()}}
-
-@foreach($reviews as $review)
-    {{$review->content}}
-@endforeach
-
-{{Form::open(['route' => ['products.reviews.store', $product]])}}
-    {{Form::textarea('content')}}
-    {{Form::submit('Create')}}
-{{Form::close()}}
+    </div>
 
 <a href="{{route('products.edit', $product)}}">Edit</a>
 
