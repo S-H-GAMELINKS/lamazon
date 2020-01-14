5ab239df0e8fb99b6fcaf6bdca2f38eed6d2cd1a
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-14 11:32:50 +0900

    fix user edit view

--- app/User.php
+++ app/User.php
@@ -17,7 +17,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password', 
+        'postal_code',
+        'address',
+        'phone'
     ];
 
     /**
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -3,8 +3,27 @@
 @section('content')
 
 {{Form::model($user, ['route' => 'mypage.update', 'method' => 'PUT'])}}
-    {{Form::textarea('name')}}
-    {{Form::submit('save')}}
+    <div class="form-group">
+        <h1>Name</h1>
+        {{Form::input('text', 'name', $user->name, ['class' => 'form-control'])}}
+    </div>
+    <div class="form-group">
+        <h1>Email</h1>
+        {{Form::input('text', 'email', $user->email, ['class' => 'form-control'])}}
+    </div>
+    <div class="form-group">
+        <h1>Postal Code</h1>
+        {{Form::input('text', 'postal_code', $user->postal_code, ['class' => 'form-control'])}}
+    </div>
+    <div class="form-group">
+        <h1>Address</h1>
+        {{Form::input('text', 'address', $user->address, ['class' => 'form-control'])}}
+    </div>
+    <div class="form-group">
+        <h1>Phone</h1>
+        {{Form::input('text', 'phone', $user->phone, ['class' => 'form-control'])}}
+    </div>
+    {{Form::submit('save', ['class' => 'btn btn-primary'])}}
 {{Form::close()}}
 
 @endsection
