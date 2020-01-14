850f7f06d5477de0981905f2d5fcd66689eb7f3f
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-14 11:15:47 +0900

    show user info

--- resources/views/users/mypage.blade.php
+++ resources/views/users/mypage.blade.php
@@ -2,7 +2,11 @@
 
 @section('content')
 
-{{$user->name}}
+<h1>{{$user->name}}</h1>
+
+<h2>{{$user->postal_code}}</h2>
+<h2>{{$user->address}}</h2>
+<h2>{{$user->phone}}</h2>
 
 <a href="{{route('mypage.edit')}}">edit</a>
 
