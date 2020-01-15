a65baf41c3846f90730bc674c71e85f723f35d52
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-15 14:39:11 +0900

    show carts

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
@@ -19,5 +21,14 @@ class CartController extends Controller
         Cart::add($request->all());
 
         return redirect()->route('products.show', $request->get('id'));
+    }
+
+    public function update(Request $request)
+    {
+        $count = DB::table('shoppingcart')->where('identifier', Auth::user()->id)->get()->count();
+
+        Cart::instance(Auth::user()->id)->store($count);
+
+        return redirect()->route('carts.index');
     }
 }
--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 
-{{$cart}}
+<div class="row d-flex justify-content-center">
+    @foreach ($cart as $product)
+    <div class="card col-3 m-3">
+        <div class="card-body">
+            <h5 class="card-title">{{$product->name}}</h5>
+            <p class="card-text">Price :{{$product->price}}  Quantity: {{$product->qty}}</p>
+            <p class="card-text">SubTotal: {{$product->subtotal}}</p>
+        </div>
+    </div>
+    @endforeach
+</div>
 
 @endsection
